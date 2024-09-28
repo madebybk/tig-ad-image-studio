3320d48841092938cfa097fd3345d0c5c9035a22
BK (Byeonghun) Kim <bhun.kim98@gmail.com>
2024-09-28 16:52:05 +0900

    add streamlit error logging

--- lib/webAppStack/userdata.sh
+++ lib/webAppStack/userdata.sh
@@ -2,6 +2,7 @@
 
 # Update packages
 sudo apt-get update -y
+sudo apt-get upgrade -y
 
 # Install required packages
 sudo apt-get install -y ec2-instance-connect
@@ -13,35 +14,41 @@ sudo apt-get install -y python3.8-venv
 cd /home/ubuntu
 git clone https://github.com/madebybk/tig-ad-image-studio.git
 
-# Create virtual environment
-sudo apt-get install -y python3.8-venv
-sudo python3 -m venv --copies /home/ubuntu/my_env
-sudo chown -R ubuntu:ubuntu /home/ubuntu/my_env
-source /home/ubuntu/my_env/bin/activate
+# Create and activate virtual environment
+python3 -m venv /home/ubuntu/streamlit_env
+source /home/ubuntu/streamlit_env/bin/activate
 
 # Install dependencies
 cd tig-ad-image-studio/frontend
-pip3 install -r requirements.txt
+pip install --upgrade pip
+pip install -r requirements.txt
 
 # Create systemd service
-sudo sh -c "cat <<EOF > /etc/systemd/system/streamlit.service
+sudo tee /etc/systemd/system/streamlit.service > /dev/null <<EOF
 [Unit]
 Description=Streamlit App
 After=network.target
 
 [Service]
 User=ubuntu
-Environment='AWS_DEFAULT_REGION=us-west-2'
+Environment="PATH=/home/ubuntu/streamlit_env/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+Environment="AWS_DEFAULT_REGION=us-west-2"
 WorkingDirectory=/home/ubuntu/tig-ad-image-studio/frontend
 ExecStartPre=/bin/bash -c 'sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8501'
-ExecStart=/bin/bash -c 'source /home/ubuntu/my_env/bin/activate && streamlit run app.py --server.port 8501 --server.enableXsrfProtection=false'
+ExecStart=/home/ubuntu/streamlit_env/bin/python -m streamlit run app.py --server.port 8501 --server.enableXsrfProtection=false
 Restart=always
+StandardOutput=append:/var/log/streamlit.log
+StandardError=append:/var/log/streamlit.log
 
 [Install]
 WantedBy=multi-user.target
-EOF"
+EOF
+
+# Create log file and set permissions
+sudo touch /var/log/streamlit.log
+sudo chown ubuntu:ubuntu /var/log/streamlit.log
 
 # Reload systemd daemon and start the service
-systemctl daemon-reload
-systemctl enable streamlit
-systemctl start streamlit
+sudo systemctl daemon-reload
+sudo systemctl enable streamlit
+sudo systemctl start streamlit
